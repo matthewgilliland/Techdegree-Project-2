aac91672ece4a21927c01e8332c6e637f3b170ec
matthewgilliland <mattgilliland88@gmail.com>
2017-07-23 18:42:20 -0400

    Fixed search/page links,  broke search page links

--- pagination-filter-v2/script.js
+++ pagination-filter-v2/script.js
@@ -14,29 +14,31 @@ let pagination = $('.pagination');
 // Global variables 2: Search bar, search result variable, name/email selection
 let searchBar = '<div class="student-search"><input id="search-input" type="text" placeholder="Search for students..."><button id="search-button">Search</button><div>';
 let searchResult;
-let currentList = '';
+let currentList = studentList;
 let names = $('h3');
 let emails = $('.email');
 
-/*FIXME: Took the showPage function from one variable to
-two when I added the search bar, but now the page
-links from addPages don't work, and I have no idea why */
 // Function to add page links
-function addPages() {
+function addPages(x) {
   let ul = $('<ul></ul>'); // Adds the ul tags
-  let pageCount = Math.ceil(currentList.length/itemCount); // Calculates page amount based on current list
+  let currentLength = currentList.length;
+  let pageCount = Math.ceil(currentLength/itemCount); // Calculates page amount based on current list
   for (let i = 1; i <= pageCount; i++) { // Loops to create each link
     let listLink = $('<li><a href="#" class="pageLink">' + i + '</a></li>'); // Produces link html
     $(ul).append(listLink); // Adds link to list
     pagination.append(ul); // Displays links to page
   }
+  $('.pageLink').click(function() { // Listens for click on page links
+    let pageNumber = event.target.textContent; // Assigns range to function by page
+    showPage(pageNumber, x); // Calls function for that page and current list
+  });
 }
-
 
 // Function to show students by page number
 function showPage(x, y) { // Takes arguments for page number and source list
   $(studentList).hide(); // Hides initial list of all students
-  for (let i = 0; i < y.length; i++) { // Loops to check for right range
+  let currentLength = y.length;
+  for (let i = 0; i < currentLength; i++) { // Loops to check for right range
     if (i < x * itemCount && i + 1 > (x - 1) * itemCount) {
       $(y[i]).show(); // Shows students in correct number range
     }
@@ -44,26 +46,21 @@ function showPage(x, y) { // Takes arguments for page number and source list
   currentList = y; // Sets current list so addPages knows how many pages to make
 }
 
-// Event listener for showing chosen page
-$('.pageLink').click(function() { // Listens for click on page links
-  let pageNumber = event.target.textContent; // Assigns range to function by page
-  showPage(pageNumber, currentList); // Calls function for that page and current list
-});
+// Show page 1 on site load
+showPage(1, studentList); // Shows default page (1) on page load w/ full list
+
+
 
 // Add search bar via JavaScript
 $('.page-header').append(searchBar); // Adds unobtrusive search bar
 
-/* FIXME: This works... but the opposite way and I don't know why. when
-I search, it removes the searched person from the screen and removes
-the links, but never shows results or replaces the links as
-the function indicates*/
 // Search function
 function searchList() {
   let searchResult = []; // Assigns searchResult to empty array/clears previous search
   let searchInput = $('#search-input').val().toLowerCase(); // Sets search to lower-case
-  pagination.remove(); // Clears links to get them out of the way
+  pagination.empty(); // Clears links to get them out of the way
   for (let i = 0; i < studentList.length; i++) {
-    if (names[i].textContent.search(searchInput) || emails[i].textContent.search(searchInput)) {
+    if (names[i].textContent.search(searchInput) != -1 || emails[i].textContent.search(searchInput) != -1) {
       searchResult.push(studentList[i]); // Loops to search name/email and add to result array
     }
   }
@@ -81,6 +78,7 @@ function searchList() {
     showPage(1, searchResult);
     addPages();
   }
+  document.getElementById('search-input').value = '';
 }
 
 // Event listener for search button
@@ -88,7 +86,6 @@ $('#search-button').click(function() {
   searchList();
 });
 
-// Show page 1 on site load
-showPage(1, studentList); // Shows default page (1) on page load w/ full list
 
-addPages(); // Calls function to actually place links on the page
+
+addPages(currentList); // Calls function to actually place links on the page
